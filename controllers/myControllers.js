ce934a32bfa8f7aa8a925a940b0764898522a5b1
Axel <axelfuh@gmail.com>
2017-05-28 17:00:13 +0200

    Add views

--- controllers/myControllers.js
+++ controllers/myControllers.js
@@ -9,10 +9,19 @@
 // const axios = require('axios');
 // const multer = require('multer');
 
+
+exports.showLogin = (req, res) => {
+  res.render('login', { title: "Login" });
+}
+
 exports.showMain = (req, res) => {
-  res.render('main', { title: "Main section" });
+  res.render('main', { title: "Home" });
 }
 
 exports.showUser = (req, res) => {
-  res.render('user', { title: "User section" });
+  res.render('user', { title: "User" });
+}
+
+exports.showEditAccount = (req, res) => {
+  res.render('account', { title: "Account" });
 }
--- routes.js
+++ routes.js
@@ -5,7 +5,10 @@ const _ = require('./controllers/myControllers');
 const router = express.Router();
 
 router.get('/',  _.showMain);
+router.get('/login',  _.showLogin);
+
 router.get('/user',  _.showUser);
+router.get('/user/edit',  _.showEditAccount);
 
 
 module.exports = router;
